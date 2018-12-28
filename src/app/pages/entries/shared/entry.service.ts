914c39b025e617a7f809b57def8c7cd57cd8ed17
Denner Lemos <denner@stj.jus.br>
2018-12-28 13:21:28 -0200

    Criação do modulo de lançamentos

---
+++ src/app/pages/entries/shared/entry.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
+
+import { Entry } from './entry.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EntryService {
+
+  private apiPath = 'api/entries';
+
+  constructor(
+    private http: HttpClient
+  ) {  }
+
+  getAll():  Observable<Entry[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handlerErro),
+      map(this.jsonDataToEntries)
+    );
+  }
+
+  getById(id: number): Observable<Entry> {
+    return this.http.get(`${this.apiPath}/${id}`).pipe(
+      catchError(this.handlerErro),
+      map(this.jsonDataToEntry)
+    );
+  }
+
+  create(entry: Entry): Observable<Entry> {
+    return this.http.post(this.apiPath, entry).pipe(
+      catchError(this.handlerErro),
+      map(this.jsonDataToEntry)
+    );
+  }
+
+  update(entry: Entry): Observable<Entry> {
+    return this.http.put(this.apiPath, entry).pipe(
+      catchError(this.handlerErro),
+      map(() => entry)
+    );
+  }
+
+  delete(id: number): Observable<any> {
+    return this.http.delete(`${this.apiPath}/${id}`).pipe(
+      catchError(this.handlerErro),
+      map(() => null)
+    );
+  }
+
+  // Metods Privados
+
+  private jsonDataToEntries(jsonData: any[]): Entry[] {
+    const entries: Entry[] = [];
+    jsonData.forEach(element => entries.push(element as Entry));
+    return entries;
+  }
+
+  private jsonDataToEntry(jsonData: any): Entry {
+    return jsonData as Entry;
+  }
+
+  private handlerErro(error: any): Observable<any>  {
+    console.log('ERROR NA REQUISIÇÃO =>', error);
+    return throwError(error);
+  }
+}
